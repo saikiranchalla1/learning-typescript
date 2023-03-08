4668a5ca3c103b70053118c72e3161f3e507b615
Sai Kiran Challa <saikiranchalla4@gmail.com>
2023-03-07 20:18:03 -0700

    Update app.ts

--- code/1. Basics and Basic Types/basics-05-object-types/app.ts
+++ code/1. Basics and Basic Types/basics-05-object-types/app.ts
@@ -1,10 +1,18 @@
+// const person: object = {
+// const person: {} = {
+
 // const person: {
 //   name: string;
 //   age: number;
 // } = {
+
 const person = {
-  name: 'Maximilian',
+  name: 'Max',
   age: 30
 };
+
+//console.log(person);
+// console.log(person.nickname); <--errors
+
 
 console.log(person.name);
