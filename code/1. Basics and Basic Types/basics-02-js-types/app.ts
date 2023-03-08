27fc510fa2ad5595a2851977cb2ec149072882a4
Sai Kiran Challa <saikiranchalla4@gmail.com>
2023-03-07 20:08:06 -0700

    Update app.ts

--- code/1. Basics and Basic Types/basics-02-js-types/app.ts
+++ code/1. Basics and Basic Types/basics-02-js-types/app.ts
@@ -1,4 +1,5 @@
 function add(n1: number, n2: number) {
+  // console.log(typeof n1);
   // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
   //   throw new Error('Incorrect input!');
   // }
