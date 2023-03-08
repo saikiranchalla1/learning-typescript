cead40e38e32aa7a42b5916b87369477b5b3aaa0
Sai Kiran Challa <saikiranchalla4@gmail.com>
2023-03-07 20:05:05 -0700

    Update app.ts

--- code/1. Basics and Basic Types/basics-01-ts-benefits/app.ts
+++ code/1. Basics and Basic Types/basics-01-ts-benefits/app.ts
@@ -1,7 +1,13 @@
+/**
+function add(n1, n2) {
+  return n1 + n2;
+}
+**/
 function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
+// const number1 = '5';
 const number1 = 5;
 const number2 = 2.8;
 
